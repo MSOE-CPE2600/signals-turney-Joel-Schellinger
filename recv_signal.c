27bb4263e543d10659b8db1f03b083a668998336
Joel-Schellinger <schellingerjf@msoe.edu>
2024-11-21 22:09:23 -0600

    Finished recv_signal.c

--- recv_signal.c
+++ recv_signal.c
@@ -4,3 +4,30 @@
  * 
  * Brief summary of program:
  */
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stddef.h>
+
+void handler(int sig, siginfo_t *info, void *context);
+
+int main() {
+    pid_t pid = getpid();
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = handler;
+    printf("PID: %d\n", pid);
+    sigaction(SIGUSR1, &sa, NULL);
+
+    while(1) {
+        pause();
+    }
+}
+
+
+void handler(int sig, siginfo_t *info, void *context) {
+    if (sig == SIGUSR1) {
+        printf("Received %d from %d\n", info->si_value.sival_int, info->si_pid);
+    }
+}
