1f4687e6e0f59291b8cdb1bf8b6eea193fda9f18
Joel-Schellinger <schellingerjf@msoe.edu>
2024-11-21 22:42:54 -0600

    Finished signal_alarm.c

--- signal_alarm.c
+++ signal_alarm.c
@@ -1,6 +1,29 @@
 /**
- * File: 
- * Modified by:
- * 
- * Brief summary of program:
+ * @file signal_alarm.c
+ * @brief Sets an alarm for 5 seconds and notifies user
+ * @author Joel Schellinger
  */
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+void handle_alarm();
+
+int main() {
+    signal(SIGALRM, handle_alarm); // Handler for SIGALRM
+    alarm(5); // Set alarm
+
+    printf("Set an alarm for 5 seconds.\n");
+    while(1) { // Wait for alarm
+        pause();
+    } 
+    return 0;
+
+}
+
+void handle_alarm() {
+    // Notify user that alarm signal was received
+    printf("Program received SIGALRM\n"); 
+    exit(0);
+}
