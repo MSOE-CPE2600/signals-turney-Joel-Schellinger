df1f4ea9bcccf849bac02ce53324cb77921aad24
Joel-Schellinger <schellingerjf@msoe.edu>
2024-11-21 22:43:29 -0600

    Finished signal_segfault.c

--- signal_segfault.c
+++ signal_segfault.c
@@ -1,18 +1,36 @@
 /**
  * @file signal_segfault.c
- * @brief Program that dereferences a null pointer causing a segmentation fault
+ * @brief Starts GDB when SIGSEGV is sent to the process
+ * @author Joel Schellinger
+ * Compile Instructions: gcc -g -o signal_segfault signal_segfault.c
  */
-
-/**
- * Modified by:
- * 
- * Brief summary of modifications:
- */
-
 
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main (int argc, char* argv[]) {
+// Signal handler to launch GDB and pause execution
+void handle_signal() {
+    char pid[16];
+    sprintf(pid, "%d", getpid());
+
+    // Notify the user
+    printf("Caught SIGSEGV. Launching GDB.\n");
+
+    // Use absolute binary path and force symbol loading
+    // Maybe later see if there's a way to get the current loaded symbols
+    // and get to the line before the segfault...
+    if(execl("/usr/bin/gdb", "gdb", "./signal_segfault", (char *)NULL) == -1){
+        // If execl fails, exit the program
+        perror("execl failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(void) {
+    signal(SIGSEGV, handle_signal);
+
     // Declare a null pointer
     int* i = NULL;
 
